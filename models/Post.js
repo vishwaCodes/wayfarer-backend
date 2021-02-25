f4a924ea1b54ebafc1b84ad9755abdaa3c750884
luvycunanan <luvycunanan@gmail.com>
2021-02-24 23:28:43 -0800

    updates user model and added user in post model

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true, minlength: 1,maxlength: 200},
   author: {
-    type: String,
+    type: Schema.Types.ObjectId,
+    ref: 'User',
   },
   img: { type: String },
   body: { type: String, required: true, trim: true },
--- models/User.js
+++ models/User.js
@@ -1,22 +1,32 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema(
-//   {
-//     username: { type: String, require: true },
-//     password: { type: String, required: true },
-//     email: {
-//       type: String,
-//       required: true,
-//     },
-//     posts: [
-//       {
-//         type: Schema.Types.ObjectId,
-//         ref: 'Post',
-//       },
-//     ],
-//   },
-//   { timestamps: true }
-// );
+const userSchema = new mongoose.Schema(
+  {
+    username: { 
+      type: String, 
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: { 
+      type: String, 
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-// const User = mongoose.model('User', userSchema);
-// module.exports = User;
+const User = mongoose.model('User', userSchema);
+module.exports = User;
